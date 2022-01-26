62a9a4e366a6c7c2e5f79af0d54ba670a61abf0c
jmumbru99 <jaimemumbru99@gmail.com>
2022-01-26 16:55:51 +0100

    Preguntas 5 y 7, refactorizacion y mockito

--- src/test/java/es/upm/grise/profundizacion/customer/smokeTest.java
+++ src/test/java/es/upm/grise/profundizacion/customer/smokeTest.java
@@ -1,6 +1,14 @@
 package es.upm.grise.profundizacion.customer;
 
 import org.junit.jupiter.api.Test;
+import static org.mockito.Mockito.*;
+import es.upm.grise.profundizacion.customer.CustomerDataAccessSingleton;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class smokeTest {
 
@@ -11,4 +19,35 @@ public class smokeTest {
 		Customer customer = new Customer(CUSTOMERID);
 	}
 
+	@Test
+	public void checkaddressestest1() throws CustomerException, DatabaseException, NoAddressException, EmptyAddressException  {
+		
+        int CUSTOMERID = 1;
+		
+		//Customer customer = new Customer(CUSTOMERID);
+		
+		CustomerDataAccessSingleton c = mock(CustomerDataAccessSingleton.class);
+		when(c.getInvoicingAddress(CUSTOMERID)).thenReturn("C/Neptuno");
+		when(c.getShippingAddress(CUSTOMERID)).thenReturn("C/Neptuno");
+		Customer customer = new Customer(CUSTOMERID);
+		assertTrue(customer.checkAddresses());
+	
+	
+	}
+	
+	@Test
+	public void checkaddressestest2() throws CustomerException, DatabaseException, NoAddressException, EmptyAddressException  {
+		
+		int CUSTOMERID = 1;
+		CustomerDataAccessSingleton c = mock(CustomerDataAccessSingleton.class);
+		when(c.getInvoicingAddress(CUSTOMERID)).thenReturn("C/Venus");
+		when(c.getShippingAddress(CUSTOMERID)).thenReturn("C/Neptuno");
+		Customer customer = new Customer(CUSTOMERID);
+		assertFalse(customer.checkAddresses());
+	
+	
+	}
+	
+	
+	
 }
