001dc9c2836500bdbffd9afb66e7a344e7ee733e
Jaime Fomperosa <jaime.fompe@gmail.com>
2022-01-26 17:13:46 +0100

    Tests cobertura

--- src/test/java/es/upm/grise/profundizacion/customer/smokeTest.java
+++ src/test/java/es/upm/grise/profundizacion/customer/smokeTest.java
@@ -1,5 +1,8 @@
 package es.upm.grise.profundizacion.customer;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import org.junit.jupiter.api.Test;
 
 public class smokeTest {
@@ -9,6 +12,30 @@ public class smokeTest {
 		int CUSTOMERID = 1;
 		
 		Customer customer = new Customer(CUSTOMERID, "", "");
+	}
+
+	@Test
+	public void equalAddresses_returnTrue() throws NoAddressException, EmptyAddressException {
+		int CUSTOMERID = 1;
+		Customer customer = new Customer(CUSTOMERID, "Calle Falsa 123", "Calle Falsa 123");
+
+		assertTrue(customer.checkAddresses());
+	}
+
+	@Test
+	public void shorterShippingAddress_returnFalse() throws NoAddressException, EmptyAddressException {
+		int CUSTOMERID = 1;
+		Customer customer = new Customer(CUSTOMERID, "Calle Falsa", "Calle Falsa 123");
+
+		assertFalse(customer.checkAddresses());
+	}
+
+	@Test
+	public void equalAddresses_returnFalse() throws NoAddressException, EmptyAddressException {
+		int CUSTOMERID = 1;
+		Customer customer = new Customer(CUSTOMERID, "Calle Verdadera 123", "Calle Falsa 123");
+
+		assertFalse(customer.checkAddresses());
 	}
 
 }
