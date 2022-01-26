8442e26428a638a05e5bd554d42d80d669528ead
DESKTOP-OCAN2B8\astan <adrix1341@gmail.com>
2022-01-26 16:51:57 +0100

    Commit de la parte de Mockito, Pregunta 7 (No realizado Pregunta 5)

--- src/test/java/es/upm/grise/profundizacion/customer/smokeTest.java
+++ src/test/java/es/upm/grise/profundizacion/customer/smokeTest.java
@@ -1,6 +1,12 @@
 package es.upm.grise.profundizacion.customer;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+import es.upm.grise.profundizacion.customer.CustomerDataAccessSingleton;
 
 public class smokeTest {
 
@@ -9,6 +15,26 @@ public class smokeTest {
 		int CUSTOMERID = 1;
 		
 		Customer customer = new Customer(CUSTOMERID);
+		
 	}
-
+	
+	@Test
+	public void direcciones_iguales() throws CustomerException, DatabaseException, NoAddressException, EmptyAddressException {
+		int CUSTOMERID = 1;
+		CustomerDataAccessSingleton cda = mock(CustomerDataAccessSingleton.class);
+	    when(cda.getInvoicingAddress(CUSTOMERID)).thenReturn("C/Ora");
+	    when(cda.getShippingAddress(CUSTOMERID)).thenReturn("C/Ora");
+	    Customer customer = new Customer(CUSTOMERID);
+	    assertTrue(customer.checkAddresses());
+	}
+	
+	@Test
+	public void direcciones_distintas() throws CustomerException, DatabaseException, NoAddressException, EmptyAddressException {
+		int CUSTOMERID = 1;
+		CustomerDataAccessSingleton cda = mock(CustomerDataAccessSingleton.class);
+	    when(cda.getInvoicingAddress(CUSTOMERID)).thenReturn("C/Ora");
+	    when(cda.getShippingAddress(CUSTOMERID)).thenReturn("C/Santander");
+	    Customer customer = new Customer(CUSTOMERID);
+	    assertTrue(!customer.checkAddresses());
+	}
 }
