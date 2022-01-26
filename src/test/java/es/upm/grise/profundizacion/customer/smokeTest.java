9e4051a75f76351de7686547bada868873f4f7c7
alvaro <elvarox301@gmail.com>
2022-01-26 16:54:38 +0100

    tests adicionales

--- src/test/java/es/upm/grise/profundizacion/customer/smokeTest.java
+++ src/test/java/es/upm/grise/profundizacion/customer/smokeTest.java
@@ -2,13 +2,32 @@ package es.upm.grise.profundizacion.customer;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class smokeTest {
 
 	@Test
 	public void test() throws CustomerException, DatabaseException {
 		int CUSTOMERID = 1;
-		
-		Customer customer = new Customer(CUSTOMERID);
+		Customer customer = new Customer(CUSTOMERID, "pepe", "pepe");
+	}
+
+	@Test
+	public void testBucle1() throws CustomerException, DatabaseException, EmptyAddressException, NoAddressException {
+		int CUSTOMERID = 1;
+		// Mockito.when(CustomerDataAccessSingleton.getInstance().getShippingAddress(CUSTOMERID)).thenReturn("pepe");
+		// Mockito.when(CustomerDataAccessSingleton.getInstance().getInvoicingAddress(CUSTOMERID)).thenReturn("pepe");
+
+		Customer customer = new Customer(CUSTOMERID, "pepe", "pepe");
+		assertEquals(customer.checkAddresses(), true);
+	}
+
+
+	@Test
+	public void testBucle2() throws CustomerException, DatabaseException, EmptyAddressException, NoAddressException {
+		int CUSTOMERID = 1;
+		Customer customer = new Customer(CUSTOMERID, "1pepe", "2pepe");
+		assertEquals(customer.checkAddresses(), false);
 	}
 
 }
