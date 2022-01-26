4a631409ba077d27f54390d5814ae68037aeabec
pablogcalonge <pablogc1999@gmail.com>
2022-01-26 16:54:00 +0100

    Tests realizados

--- src/test/java/es/upm/grise/profundizacion/customer/smokeTest.java
+++ src/test/java/es/upm/grise/profundizacion/customer/smokeTest.java
@@ -1,14 +1,34 @@
 package es.upm.grise.profundizacion.customer;
 
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+
+import static org.mockito.Mockito.mock;
 
 public class smokeTest {
 
+	//@Mock
+	//Customer customer;
+
+	@Mock
+	CustomerDataAccessSingleton customerDataAccessSingleton;
+
+	/*
 	@Test
 	public void test() throws CustomerException, DatabaseException {
 		int CUSTOMERID = 1;
 		
 		Customer customer = new Customer(CUSTOMERID);
+	}*/
+
+	@Test
+	public void checkAddressesMismoString(){
+		Customer customer = new Customer(1, "gato", "gato");
+	}
+
+	@Test
+	public void checkAddressesDistintoStringMismaLong(){
+		Customer customer = new Customer(1, "gato", "pera");
 	}
 
 }
